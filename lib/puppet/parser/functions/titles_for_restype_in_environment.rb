20dc422dfea210f75cba695252fb16412687b068
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-09-26 13:10:55 +0200

    A first attempt to also update titles_for_restype_in_environment to Puppet 3 logic

--- lib/puppet/parser/functions/titles_for_restype_in_environment.rb
+++ lib/puppet/parser/functions/titles_for_restype_in_environment.rb
@@ -1,19 +1,26 @@
 require "puppet"
-require "puppet/rails"
 
 module Puppet::Parser::Functions
     newfunction(:titles_for_restype_in_environment, :type => :rvalue ) do |args|
         theEnvironment = args[0]
         theRestype = args[1]
     
-        class Resources < Puppet::Rails::Resource; end
-        printtype = "title"
-        query = "resources.restype = \'" + theRestype + "\' AND hosts.environment = \'" + theEnvironment + "\'"
-        Resources.find(
-                :all,
-                :include => [ :host ],
-                :conditions => query
-            ).map { |resource| resource.send(printtype) }
+        Puppet::Parser::Functions.autoloader.load(:versioncmp) unless Puppet::Parser::Functions.autoloader.loaded?(:versioncmp)
+
+        if lookupvar("puppetversion")[0] == "2" then
+            require "puppet/rails"
+            class Resources < Puppet::Rails::Resource; end
+            printtype = "title"
+            query = "resources.restype = \'" + theRestype + "\' AND hosts.environment = \'" + theEnvironment + "\'"
+            Resources.find(
+                    :all,
+                    :include => [ :host ],
+                    :conditions => query
+                ).map { |resource| resource.send(printtype) }
+        else
+            Puppet::Parser::Functions.autoloader.load(:query_nodes) unless Puppet::Parser::Functions.autoloader.loaded?(:query_nodes)
+            function_query_nodes([theRestype.to_s + ' and environment="' + theEnvironment + '"', 'fqdn'])
+        end
     end
 end
 
